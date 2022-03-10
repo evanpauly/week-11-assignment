2da9926568b86e2fce6c7bb5fcbd3339c81afd3c
evanpauly <94558036+evanpauly@users.noreply.github.com>
2022-03-10 11:30:37 -0600

    trying something

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,9 +2,17 @@ const router = require('express').Router();
 const data = require('../db/db.json')
 const fs = require('fs');
 
+router.get('/api/notes', (req, res) => {
+    res.json(data)
+});
 
-
-
+router.post('/api/notes', (req, res) => {
+    data.push(req.body)
+    fs.writeFile('./db/db.json', JSON.stringify(data), err => {
+        if (err) throw err;
+    })
+    res.send('Data Added')
+});
 
 
 module.exports = router
