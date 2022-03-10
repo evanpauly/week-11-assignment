c658d92a68789e4c9a874643c37547de0e68d52b
evanpauly <94558036+evanpauly@users.noreply.github.com>
2022-03-10 10:38:31 -0600

    reviewing module and class challenges for files and paths

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes');
+const app = express();
+
+app.use(express.json());
+
 const fs = require('fs');
 const db = require('./Develop/db/db.json')
 
 const PORT = process.env.PORT || 3001;
 const public = require('./Develop/public/')
-const app = express();
+
 
 app.get('/api/db', (req, res) => {
     res.json(db);
