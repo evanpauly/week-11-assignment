b7f64ef37d2256e18618f1443ece69bd0ba16a81
evanpauly <94558036+evanpauly@users.noreply.github.com>
2022-03-10 09:59:06 -0600

    setting up first steps

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const fs = require('fs');
-const { db } = require('./Develop/db/db.json')
+const db = require('./Develop/db/db.json')
 
+const PORT = process.env.PORT || 3001;
+const public = require('./Develop/public/')
 const app = express();
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.get('/api/db', (req, res) => {
+    res.json(db);
+})
+
+
+
+app.use('/', public);
+
+app.listen(PORT, () => {
+    console.log(`API server now on ${PORT}!`);
 });
