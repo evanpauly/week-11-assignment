9900d7b8bb54c09609bb5cb3cd1c205de0041a56
evanpauly <94558036+evanpauly@users.noreply.github.com>
2022-03-10 13:42:31 -0600

    trying to figure out buttons

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,12 +1,14 @@
 const router = require('express').Router();
 const data = require('../db/db.json')
 const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 
 router.get('/api/notes', (req, res) => {
     res.json(data)
 });
 
 router.post('/api/notes', (req, res) => {
+    req.body.id = uuidv4();
     data.push(req.body)
     fs.writeFile('./db/db.json', JSON.stringify(data), err => {
         if (err) throw err;
@@ -14,5 +16,13 @@ router.post('/api/notes', (req, res) => {
     res.send('Data Added')
 });
 
+router.delete('/notes/:id', (req, res) => {
+    const createdNote = notes;
+    const idNotes = createdNote.filter(note => note.id != req.params.id);
+    fs.writeFileSync(path.join(__dirname, '../../db/db.json'), JSON.stringify(idNotes), null, 2);
+
+    res.json(idNotes);
+    notes = idNotes;
+})
 
 module.exports = router
