f27778e8fdeeadd7191e79d49e5deb18c56d202e
evanpauly <94558036+evanpauly@users.noreply.github.com>
2022-03-10 13:49:46 -0600

    will work on delete button later

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const data = require('../db/db.json')
 const fs = require('fs');
-const { v4: uuidv4 } = require('uuid');
+//const { v4: uuidv4 } = require('uuid');
 
 router.get('/api/notes', (req, res) => {
     res.json(data)
@@ -15,14 +15,5 @@ router.post('/api/notes', (req, res) => {
     })
     res.send('Data Added')
 });
-
-router.delete('/notes/:id', (req, res) => {
-    const createdNote = notes;
-    const idNotes = createdNote.filter(note => note.id != req.params.id);
-    fs.writeFileSync(path.join(__dirname, '../../db/db.json'), JSON.stringify(idNotes), null, 2);
-
-    res.json(idNotes);
-    notes = idNotes;
-})
 
 module.exports = router
